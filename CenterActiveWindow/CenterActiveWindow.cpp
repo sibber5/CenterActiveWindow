860474eef44f93ec17bd99fc4ef8b88647b955c3
sibber5 <ghasjado@gmail.com>
2025-02-14 21:52:35 +0200

    Fix trying to center os windows, like the desktop

--- CenterActiveWindow/CenterActiveWindow.cpp
+++ CenterActiveWindow/CenterActiveWindow.cpp
@@ -22,16 +22,16 @@ HMENU hNotifyIconMenu = NULL;
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-void ShowLastErrorMessage()
+static void ShowLastErrorMessage()
 {
     LPTSTR messageBuffer = NULL;
     FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_IGNORE_INSERTS,
         NULL, GetLastError(), LANG_USER_DEFAULT, (LPTSTR)&messageBuffer, 0, NULL);
 
-    MessageBox(NULL, messageBuffer, L"CenterActiveWindow - Error", MB_OK | MB_ICONERROR);
+    MessageBox(NULL, messageBuffer, TEXT("CenterActiveWindow - Error"), MB_OK | MB_ICONERROR);
 }
 
-bool GetPathToExe(WCHAR buffer[MAX_PATH])
+static bool GetPathToExe(WCHAR buffer[MAX_PATH])
 {
     DWORD len = GetModuleFileName(NULL, buffer, MAX_PATH);
     if (len == 0)
@@ -42,9 +42,9 @@ bool GetPathToExe(WCHAR buffer[MAX_PATH])
     return true;
 }
 
-HWND CreateMessageWindow(HINSTANCE hInstance)
+static HWND CreateMessageWindow(HINSTANCE hInstance)
 {
-    const WCHAR CLASS_NAME[] = L"CenterActiveWindow Class";
+    const WCHAR CLASS_NAME[] = TEXT("CenterActiveWindow Class");
 
     WNDCLASS wc = {};
     wc.lpfnWndProc = WindowProc;
@@ -56,7 +56,7 @@ HWND CreateMessageWindow(HINSTANCE hInstance)
     return CreateWindowEx(0, CLASS_NAME, NULL, 0, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, HWND_MESSAGE, NULL, hInstance, NULL);
 }
 
-bool CreateNotifyIcon(HWND hWnd, HINSTANCE hInstance, WCHAR pathToIcon[MAX_PATH])
+static bool CreateNotifyIcon(HWND hWnd, HINSTANCE hInstance, WCHAR pathToIcon[MAX_PATH])
 {
     NOTIFYICONDATA niData = {};
     niData.cbSize = sizeof(NOTIFYICONDATA);
@@ -68,7 +68,7 @@ bool CreateNotifyIcon(HWND hWnd, HINSTANCE hInstance, WCHAR pathToIcon[MAX_PATH]
     HICON icon = ExtractIcon(hInstance, pathToIcon, 0);
     niData.hIcon = icon;
 
-    WCHAR tooltip[] = L"Center Active Window - Win + Alt + C";
+    WCHAR tooltip[] = TEXT("Center Active Window - Win + Alt + C");
     int count = sizeof(tooltip) / sizeof(WCHAR);
     for (int i = 0; i < count; ++i)
     {
@@ -86,17 +86,87 @@ bool CreateNotifyIcon(HWND hWnd, HINSTANCE hInstance, WCHAR pathToIcon[MAX_PATH]
     return true;
 }
 
-HMENU CreateNotifyIconMenu()
+static HMENU CreateNotifyIconMenu()
 {
     HMENU hMenu = CreatePopupMenu();
     InsertMenu(hMenu, -1, MF_BYPOSITION | MF_STRING, MENU_ITEM_ID, L"Exit");
     return hMenu;
 }
 
-bool OnHotkeyPressed()
+static bool IsOSWindow(HWND window)
+{
+    // this is probably not a reliable way to check but i couldnt find another way
+
+    const int bufferLen = 64;
+    TCHAR s[bufferLen];
+    int length = GetClassName(window, s, bufferLen);
+    // if window is desktop
+    if (CompareStringOrdinal(s, length, TEXT("Progman"), 7, FALSE) == CSTR_EQUAL
+        || CompareStringOrdinal(s, length, TEXT("WorkerW"), 7, FALSE) == CSTR_EQUAL)
+    {
+        return true;
+    }
+    // if window is start or search or notif or ...
+    else if (CompareStringOrdinal(s, length, TEXT("Windows.UI.Core.CoreWindow"), 26, FALSE) == CSTR_EQUAL)
+    {
+        length = GetWindowText(window, s, bufferLen);
+        if (CompareStringOrdinal(s, length, TEXT("Windows Shell Experience Host"), 29, FALSE) == CSTR_EQUAL
+            || CompareStringOrdinal(s, length, TEXT("Windows Input Experience"), 24, FALSE) == CSTR_EQUAL
+            || CompareStringOrdinal(s, length, TEXT("Start"), 5, FALSE) == CSTR_EQUAL
+            || CompareStringOrdinal(s, length, TEXT("Search"), 6, FALSE) == CSTR_EQUAL
+            || CompareStringOrdinal(s, length, TEXT("Notification Center"), 19, FALSE) == CSTR_EQUAL
+            || CompareStringOrdinal(s, length, TEXT("Quick settings"), 14, FALSE) == CSTR_EQUAL
+            || CompareStringOrdinal(s, length, TEXT("New notification"), 16, FALSE) == CSTR_EQUAL)
+        {
+            return true;
+        }
+    }
+    // if window is taskbar
+    else if (CompareStringOrdinal(s, length, TEXT("Shell_TrayWnd"), 13, FALSE) == CSTR_EQUAL
+        || CompareStringOrdinal(s, length, TEXT("Shell_SecondaryTrayWnd"), 22, FALSE) == CSTR_EQUAL)
+    {
+        return true;
+    }
+    else if (CompareStringOrdinal(s, length, TEXT("MSTaskSwWClass"), 14, FALSE) == CSTR_EQUAL)
+    {
+        length = GetWindowText(window, s, bufferLen);
+        if (CompareStringOrdinal(s, length, TEXT("Running applications"), 20, FALSE) == CSTR_EQUAL)
+        {
+            return true;
+        }
+    }
+    else if (CompareStringOrdinal(s, length, TEXT("Windows.UI.Composition.DesktopWindowContentBridge"), 49, FALSE) == CSTR_EQUAL)
+    {
+        length = GetWindowText(window, s, bufferLen);
+        if (CompareStringOrdinal(s, length, TEXT("DesktopWindowXamlSource"), 23, FALSE) == CSTR_EQUAL)
+        {
+            return true;
+        }
+    }
+    else
+    {
+        HWND child = GetWindow(window, GW_CHILD);
+        if (child != NULL)
+        {
+            length = GetWindowText(child, s, bufferLen);
+            if (CompareStringOrdinal(s, length, TEXT("DesktopWindowXamlSource"), 23, FALSE) == CSTR_EQUAL)
+            {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+// returns: false if there was an error, which will be displayed by ShowLastErrorMessage. otherwise true.
+static bool OnHotkeyPressed()
 {
     HWND window = GetForegroundWindow();
     if (window == NULL) return true;
+
+    if (IsWindowVisible(window) == 0
+        || IsOSWindow(window)) return true;
 
     RECT windowRect = {};
     if (GetWindowRect(window, (LPRECT)&windowRect) == 0) return false;
